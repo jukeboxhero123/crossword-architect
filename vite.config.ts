e4048b429e5f2a0645cb5679a8ae1a3a75d58c28
Jerry Tan <jerrytan7@gmail.com>
2025-11-30 01:09:39 -0500

    change build result

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteSingleFile } from 'vite-plugin-singlefile'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), viteSingleFile()],
+  base: './', // Use relative paths so it works when opening HTML file directly
 })
